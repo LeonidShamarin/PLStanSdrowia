96edf2a5e831c006956e3793a86d278a4f2bdb90
Leonid <Leonideko1@gmail.com>
2024-07-17 15:55:10 +0300

    slider add

---
+++ slider-media.js
@@ -0,0 +1,13 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
